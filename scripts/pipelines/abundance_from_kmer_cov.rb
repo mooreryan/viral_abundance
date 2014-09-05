7365e25549e1e34861fa3c43c5df6fa69c549b2c
Ryan Moore <moorer@udel.edu>
2014-09-04 20:34:35 -0400

    Shorten pipeline

--- scripts/pipelines/abundance_from_kmer_cov.rb
+++ scripts/pipelines/abundance_from_kmer_cov.rb
@@ -14,11 +14,21 @@ rescue LoadError => e
   abort("ERROR: #{e.message}\nTry running: gem install #{bad_file}")
 end
 
+default_references = '/home/moorer/public/artificial_metagenomes/' +
+  'sequences/fastas/genomes/all_10.fa'
+default_scaf_seq = '/Users/ryanmoore/projects/steve/viral_abundance/' +
+  'test_files/inputs/test_127.scafSeq'
+default_blast = '/usr/bin/blastn'
+default_blast_db = '/home/moorer/public/artificial_metagenomes/' + 
+  'blast_dbs/all10.fa'
+default_outdir = '/Users/ryanmoore/projects/steve/viral_abundance/' + 
+  'test_files/output'
+
 opts = Trollop.options do
   banner <<-EOS
 
-  If the scaf_seq file is `test_127.scafSeq` then the output files will
-  be these:
+  If the scaf_seq file is `test_127.scafSeq` then the output files
+  will be these:
 
   test_127.abun_from_kmer_cov.r
   test_127.abun_from_kmer_cov.txt
@@ -38,15 +48,15 @@ opts = Trollop.options do
   Options:
   EOS
   opt(:references, 'The fasta file of reference sequences',
-      type: :string, default: '/home/moorer/public/artificial_metagenomes/sequences/fastas/genomes/all_10.fa')
+      type: :string, default: defualt_references)
   opt(:scaf_seq, 'The scafSeq file from SOAPdenovo', type: :string,
-      default: '/Users/ryanmoore/projects/steve/viral_abundance/test_files/inputs/test_127.scafSeq')
+      default: default_scaf_seq)
   opt(:blast, 'Location of your blast binary', type: :string,
-      default: '/usr/bin/blastn')
+      default: default_blast)
   opt(:blast_db, 'Path to blast db', type: :string,
-      default: '/home/moorer/public/artificial_metagenomes/blast_dbs/all10.fa')
+      default: default_blast_db)
   opt(:outdir, 'Output directory', type: :string,
-      default: '/Users/ryanmoore/projects/steve/viral_abundance/test_files/output')
+      default: default_outdir)
   opt(:reduce_coverage, 'Flag true to run coverage reduction algorithm',
       type: :boolean, default: true)
 end
@@ -203,198 +213,207 @@ $stderr.print 'Getting top hits...'
 hits = top_hit(blast_out)
 $stderr.puts "Done! (time: #{Time.now - t})"
 
-def percent_covered(array)
-  (array.count - array.count(0)) / array.count.to_f
+
+## print top hits
+puts %w[query tax-hit eval start stop length].join("\t")
+hits.each_pair do |query, info|
+  puts info.values.join("\t")
 end
 
-# TODO get reference lengths here
-# { reference_name => length }
-ref_lengths = {}
-FastaFile.open(opts[:references], 'r').each_record do |header, sequence|
-  if ref_lengths.has_key?(header)
-    abort("ERROR: #{header} isn't uniuqe in #{opts[:references]}")
-  else
-    ref_lengths[header] = sequence.length
-  end
-end
-
-def clean_tax_string(tax_str)
-  tax_str
-    .sub(/^gi.*\| /, '')
-    .sub(/, complete.*$/, '')
-    .sub(/ genomic sequence$/, '')
-end
-
-if opts[:reduce_coverage]
-  begin
-    cov_file = File.join(opts[:outdir], 
-                         "#{fname_map[:base]}.reference_coverage.txt")
-    kept_file = File.join(opts[:outdir], 
-                          "#{fname_map[:base]}.contigs_kept.txt")
-    rejected_file = 
-      File.join(opts[:outdir], 
-                "#{fname_map[:base]}.contigs_rejected.txt")
-    cov_f = File.open(cov_file, 'w')
-    kept_f = File.open(kept_file, 'w')
-    rejected_f = File.open(rejected_file, 'w')
-    cov_f.puts "name\tbase\tcov"
-
-    ## step 7.5 'coverage reduction' algorithm
-    hits.values.group_by { |record| record[:tax_hit] }
-      .each do |tax, recs|
-      tax_string = clean_tax_string(tax)
-      # fill array with zeros for length of reference sequence
-      ref_coverage = Array.new(ref_lengths[tax], 0)
-
-      # make reference sequence coverage data structure
-      # tax is the value of tax_hit from hits
-      # recs are all the top hits to this tax group
-      sorted_records = recs.sort_by { |record| record[:length] }
-      sorted_records.each do |record|
-        if percent_covered(ref_coverage) > 0.90
-          break
-        else
-          start = record[:start].to_i - 1
-          stop = record[:stop].to_i - 1
-          start, stop = stop, start if stop < start
-
-          # slice the region
-          region = (start..stop)
-          cov_of_region_on_ref = ref_coverage[region]
-          if percent_covered(cov_of_region_on_ref) < 0.5
-            # add a good flag to hits
-            hits[record[:query]][:keep] = true
-
-            # add 1 to the coverage at the proper positions
-            region.each do |posn|
-              ref_coverage[posn] += 1
-            end
-          else
-            hits[record[:query]][:keep] = false
-          end
-        end
-      end
-      ref_coverage.each_with_index do |cov, idx|
-        base = idx + 1
-        cov_f.puts [tax_string, base, cov].join("\t")
-      end
-    end
-    # hits will now be
-    # { query => { :query, :tax_hit, :eval, :start, :stop, :length } }
-    kept = hits.values.select { |hit| hit[:keep] }
-    rejected = hits.values.reject { |hit| hit[:keep] }
-    kept.each do |info|
-      info.each_pair do |k, v|
-        kept_f.puts "#{k}\t#{v}"
-      end
-    end
-    rejected.each do |info|
-      info.each_pair do |k, v|
-        rejected_f.puts "#{k}\t#{v}"
-      end
-    end
-    # hits now only contains the ones flagged with keep
-    hits = kept
-  ensure
-    cov_f.close
-    kept_f.close
-    rejected_f.close
-  end
-else
-  hits = hits.values
-end
-
-## stats per tax group
-t = Time.now
-$stderr.print 'Getting cov stats per tax group...'
-tax_cov = {}
-hits.group_by { |record| record[:tax_hit] }.each do |tax, recs|
-  cov_for_this_tax = []
-  recs.each do |rec|
-    cov_for_this_tax << records[rec[:query]][:cov]
-  end
-  tax_cov[tax] = { 
-    mean_cov: cov_for_this_tax.mean,
-    median_cov: cov_for_this_tax.median,
-    sd: cov_for_this_tax.standard_deviation,
-    count: cov_for_this_tax.count }
-end
-$stderr.puts "Done! (time: #{Time.now - t})"
-
-t = Time.now
-$stderr.print 'Printing tax cov stats data...'
-r_data = File.join(opts[:outdir], 
-                   fname_map[:base] + ".abun_from_kmer_cov.txt")
-File.open(r_data, 'w') do |f|
-  f.puts(%w[virus program kmer.size mean.cov median.cov sd count]
-           .join("\t"))
-  tax_cov.each_pair do |tax, info|
-    tax_string = clean_tax_string(tax)
-    f.puts [tax_string,
-            grinder_program,
-            kmer_size,
-            info[:mean_cov], 
-            info[:median_cov], 
-            info[:sd], 
-            info[:count]].join("\t")
-  end
-end
-$stderr.puts "Done! (time: #{Time.now - t})"
 
 
+# def percent_covered(array)
+#   (array.count - array.count(0)) / array.count.to_f
+# end
 
-n50_pdf_out = File.join(opts[:outdir], 
-                    fname_map[:base] + ".n50_table.pdf")
-rank_pdf_out = File.join(opts[:outdir], 
-                    fname_map[:base] + ".rank_abundance.pdf")
+# # TODO get reference lengths here
+# # { reference_name => length }
+# ref_lengths = {}
+# FastaFile.open(opts[:references], 'r').each_record do |header, sequence|
+#   if ref_lengths.has_key?(header)
+#     abort("ERROR: #{header} isn't uniuqe in #{opts[:references]}")
+#   else
+#     ref_lengths[header] = sequence.length
+#   end
+# end
 
-t = Time.now
-$stderr.print 'Running R code...'
-r_script =
-"library('ggplot2')
+# def clean_tax_string(tax_str)
+#   tax_str
+#     .sub(/^gi.*\| /, '')
+#     .sub(/, complete.*$/, '')
+#     .sub(/ genomic sequence$/, '')
+# end
 
-## n50 graph
-pdf(file='#{n50_pdf_out}', width=10, height=7.5)
-n50 <- read.table('#{n50_f}', header=T, sep=\"\\t\")
+# if opts[:reduce_coverage]
+#   begin
+#     cov_file = File.join(opts[:outdir], 
+#                          "#{fname_map[:base]}.reference_coverage.txt")
+#     kept_file = File.join(opts[:outdir], 
+#                           "#{fname_map[:base]}.contigs_kept.txt")
+#     rejected_file = 
+#       File.join(opts[:outdir], 
+#                 "#{fname_map[:base]}.contigs_rejected.txt")
+#     cov_f = File.open(cov_file, 'w')
+#     kept_f = File.open(kept_file, 'w')
+#     rejected_f = File.open(rejected_file, 'w')
+#     cov_f.puts "name\tbase\tcov"
 
-ggplot(n50, aes(x=level, y=length)) +
-    geom_bar(position=position_dodge(), 
-             stat='identity', fill='dodgerblue2', col='black' ) +
-    xlab('X percent of bases contained in contig of length of at least Y') +
-    ylab('Contig length') +
-    labs(title='Assembly metrics #{opts[:scaf_seq]}') +
-    guides(fill=F) +
-    theme_bw()
-invisible(dev.off())
+#     ## step 7.5 'coverage reduction' algorithm
+#     hits.values.group_by { |record| record[:tax_hit] }
+#       .each do |tax, recs|
+#       tax_string = clean_tax_string(tax)
+#       # fill array with zeros for length of reference sequence
+#       ref_coverage = Array.new(ref_lengths[tax], 0)
 
-## rank abundance
-pdf(file='#{rank_pdf_out}', width=10, height=7.5)
-t <- read.table('#{r_data}', header=T, sep=\"\\t\")
-ggplot(t, aes(x=virus, y=mean.cov)) +
-    geom_bar(position=position_dodge(), stat='identity', 
-             fill='dodgerblue2', col='black') +
-    xlab('Virus') +
-    ylab('Relative abundance') +
-    labs(title='Rank abundance #{opts[:scaf_seq]}') +
-    guides(fill=F) +
-    theme_bw() +
-    theme(axis.text.x = element_text(angle=45, vjust=0.5, size=8))
-invisible(dev.off())
-"
+#       # make reference sequence coverage data structure
+#       # tax is the value of tax_hit from hits
+#       # recs are all the top hits to this tax group
+#       sorted_records = recs.sort_by { |record| record[:length] }
+#       sorted_records.each do |record|
+#         if percent_covered(ref_coverage) > 0.90
+#           break
+#         else
+#           start = record[:start].to_i - 1
+#           stop = record[:stop].to_i - 1
+#           start, stop = stop, start if stop < start
 
-tmp_r_script = File.join(opts[:outdir], 
-                         fname_map[:base] + ".abun_from_kmer_cov.r")
-File.open(tmp_r_script, 'w') do |f|
-  f.puts r_script
-end
+#           # slice the region
+#           region = (start..stop)
+#           cov_of_region_on_ref = ref_coverage[region]
+#           if percent_covered(cov_of_region_on_ref) < 0.5
+#             # add a good flag to hits
+#             hits[record[:query]][:keep] = true
 
-# run the rcode
-begin
-  Shell.execute!("Rscript #{tmp_r_script}")
-rescue RuntimeError => e
-  # print stderr if bad exit status
-  abort(e.message)
-end
-$stderr.puts "Done! (time: #{Time.now - t})"
+#             # add 1 to the coverage at the proper positions
+#             region.each do |posn|
+#               ref_coverage[posn] += 1
+#             end
+#           else
+#             hits[record[:query]][:keep] = false
+#           end
+#         end
+#       end
+#       ref_coverage.each_with_index do |cov, idx|
+#         base = idx + 1
+#         cov_f.puts [tax_string, base, cov].join("\t")
+#       end
+#     end
+#     # hits will now be
+#     # { query => { :query, :tax_hit, :eval, :start, :stop, :length } }
+#     kept = hits.values.select { |hit| hit[:keep] }
+#     rejected = hits.values.reject { |hit| hit[:keep] }
+#     kept.each do |info|
+#       info.each_pair do |k, v|
+#         kept_f.puts "#{k}\t#{v}"
+#       end
+#     end
+#     rejected.each do |info|
+#       info.each_pair do |k, v|
+#         rejected_f.puts "#{k}\t#{v}"
+#       end
+#     end
+#     # hits now only contains the ones flagged with keep
+#     hits = kept
+#   ensure
+#     cov_f.close
+#     kept_f.close
+#     rejected_f.close
+#   end
+# else
+#   hits = hits.values
+# end
+
+# ## stats per tax group
+# t = Time.now
+# $stderr.print 'Getting cov stats per tax group...'
+# tax_cov = {}
+# hits.group_by { |record| record[:tax_hit] }.each do |tax, recs|
+#   cov_for_this_tax = []
+#   recs.each do |rec|
+#     cov_for_this_tax << records[rec[:query]][:cov]
+#   end
+#   tax_cov[tax] = { 
+#     mean_cov: cov_for_this_tax.mean,
+#     median_cov: cov_for_this_tax.median,
+#     sd: cov_for_this_tax.standard_deviation,
+#     count: cov_for_this_tax.count }
+# end
+# $stderr.puts "Done! (time: #{Time.now - t})"
+
+# t = Time.now
+# $stderr.print 'Printing tax cov stats data...'
+# r_data = File.join(opts[:outdir], 
+#                    fname_map[:base] + ".abun_from_kmer_cov.txt")
+# File.open(r_data, 'w') do |f|
+#   f.puts(%w[virus program kmer.size mean.cov median.cov sd count]
+#            .join("\t"))
+#   tax_cov.each_pair do |tax, info|
+#     tax_string = clean_tax_string(tax)
+#     f.puts [tax_string,
+#             grinder_program,
+#             kmer_size,
+#             info[:mean_cov], 
+#             info[:median_cov], 
+#             info[:sd], 
+#             info[:count]].join("\t")
+#   end
+# end
+# $stderr.puts "Done! (time: #{Time.now - t})"
+
+
+
+# n50_pdf_out = File.join(opts[:outdir], 
+#                     fname_map[:base] + ".n50_table.pdf")
+# rank_pdf_out = File.join(opts[:outdir], 
+#                     fname_map[:base] + ".rank_abundance.pdf")
+
+# t = Time.now
+# $stderr.print 'Running R code...'
+# r_script =
+# "library('ggplot2')
+
+# ## n50 graph
+# pdf(file='#{n50_pdf_out}', width=10, height=7.5)
+# n50 <- read.table('#{n50_f}', header=T, sep=\"\\t\")
+
+# ggplot(n50, aes(x=level, y=length)) +
+#     geom_bar(position=position_dodge(), 
+#              stat='identity', fill='dodgerblue2', col='black' ) +
+#     xlab('X percent of bases contained in contig of length of at least Y') +
+#     ylab('Contig length') +
+#     labs(title='Assembly metrics #{opts[:scaf_seq]}') +
+#     guides(fill=F) +
+#     theme_bw()
+# invisible(dev.off())
+
+# ## rank abundance
+# pdf(file='#{rank_pdf_out}', width=10, height=7.5)
+# t <- read.table('#{r_data}', header=T, sep=\"\\t\")
+# ggplot(t, aes(x=virus, y=mean.cov)) +
+#     geom_bar(position=position_dodge(), stat='identity', 
+#              fill='dodgerblue2', col='black') +
+#     xlab('Virus') +
+#     ylab('Relative abundance') +
+#     labs(title='Rank abundance #{opts[:scaf_seq]}') +
+#     guides(fill=F) +
+#     theme_bw() +
+#     theme(axis.text.x = element_text(angle=45, vjust=0.5, size=8))
+# invisible(dev.off())
+# "
+
+# tmp_r_script = File.join(opts[:outdir], 
+#                          fname_map[:base] + ".abun_from_kmer_cov.r")
+# File.open(tmp_r_script, 'w') do |f|
+#   f.puts r_script
+# end
+
+# # run the rcode
+# begin
+#   Shell.execute!("Rscript #{tmp_r_script}")
+# rescue RuntimeError => e
+#   # print stderr if bad exit status
+#   abort(e.message)
+# end
+# $stderr.puts "Done! (time: #{Time.now - t})"
 
 $stderr.puts "All done!!! (total time: #{Time.now - start_time})"
